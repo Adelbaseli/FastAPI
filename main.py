8825c3cc573ea1f08e1452eb8b9e0a32919988e7
Adel Baselizadeh <adel.baselizadeh@gmail.com>
2026-07-12 14:46:05 +0200

    010

--- main.py
+++ main.py
@@ -34,10 +34,49 @@ def create_user(user: UserRequest):
 def get_users():
     return users
 
-@app.get('/user/{user_id}', response_model=UserOutput)
+@app.get('/user/id/{user_id}', response_model=UserOutput) 
 def get_user(user_id: int):
     user = next((u for u in users if user_id == u.id), None)
     if not user:
         raise HTTPException(status_code = status.HTTP_404_BAD_REQUEST,
                            detail="User not found")
     return user
+
+
+@app.put('/user/{user_id}', response_model=UserResponse)
+def update(user_id: int, user: UserRequest):
+    existing_user = next((u for u in users if u.id == user_id), None)
+    if not existing_user:
+       return create_user(user)
+    
+    for key , value in user.model_dump().items():
+        if hasattr(existing_user, key):
+            setattr(existing_user, key, value)
+            
+    return({"message": "User updated",
+           "user": existing_user})
+
+
+@app.patch('/user/{user_id}', response_model=UserResponse)
+def patch_user(user_id: int, user: UserRequest):
+    existing_user = next((u for u in users if u.id == user_id), None)
+    if not existing_user:
+       return create_user(user)
+
+    for key , value in user.model_dump().items():
+        if hasattr(existing_user, key) and value is not None:
+            setattr(existing_user, key, value)
+
+    return({"message": "user updated",
+            "user": existing_user})   
+
+@app.delete('/user/{user_id}')
+def user_delete(user_id: int):
+    existing_user = next((u for u in users if user_id == u.id) , None)
+    if not existing_user:
+        raise HTTPException(status_code = status.HTTP_404_BAD_REQUEST,
+                           detail="User not found")
+    users.remove(existing_user)
+    for u in users:
+        if u.id > user_id:
+            u.id -= 1      
--- schemas.py
+++ schemas.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, Field, EmailStr
-
+from typing import Optional
 class UserRequest(BaseModel):
     name: str = Field(..., min_length=3, max_length=50, description = "The name of the user")
     age: int = Field(..., ge=1, le=120, description="The user's age")
     email: EmailStr = Field(..., format='email', description="User's email")
     password: str = Field(..., min_length=3, max_legth=15, description="User's password")
 
+class UserPatchRequest(BaseModel):
+    name: Optional[str] = Field(None, min_length=3, max_length=50, description = "The name of the user")
+    age: Optional[int] = Field(None, ge=1, le=120, description="The user's age")
+    email: Optional[EmailStr] = Field(None, format='email', description="User's email")
+    password: Optional[str] = Field(None, min_length=3, max_legth=15, description="User's password")
 
 class UserOutput(BaseModel):
     id: int = Field(...,description="This is the user ID")
